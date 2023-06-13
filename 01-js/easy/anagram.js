8ab5522adf7abfe68f93f390c0400d87e1a7b9e7
gauravpratapsingh <nikumbgo@gmail.com>
2023-06-13 21:14:07 +0530

    anagram js solution easy terms learned
     arrayCompare = test1.every(function (element,index) {
            return element === test2[index];
        })
    
     string1.split("").forEach(chr => {
             test[chr.charCodeAt(0) - 65] = test[chr.charCodeAt(0) - 65] + 1
         });

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,32 @@
   - `npm run test-anagram`
 */
 
+//Logic assuming all characters are string literals
 function isAnagram(str1, str2) {
+    str1 = str1.toUpperCase();
+    str2 = str2.toUpperCase();
+    if (str1.length != str2.length)
+        return false;
+    test1 = charArray(str1);
+    test2 = charArray(str2);
+
+    arrayCompare = test1.every(function (element,index) {
+        return element === test2[index];
+    })
+
+    return arrayCompare
+}
+
+function charArray(string1) {
+    var test = new Array(26).fill(0);
+    string1.split("").forEach(chr => {
+        test[chr.charCodeAt(0) - 65] = test[chr.charCodeAt(0) - 65] + 1
+    });
+
+    return test;
 
 }
 
+
+//console.log(isAnagram("test!233", "test@#$#"))
 module.exports = isAnagram;
