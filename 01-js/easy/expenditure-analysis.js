57d0fc3dee11bb53fbf2b121e6c6a1a4becc2199
gauravpratapsingh <nikumbgo@gmail.com>
2023-06-13 23:54:21 +0530

    easy 2 total spent category wise

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -8,8 +8,33 @@
   - `npm run test-expenditure-analysis`
 */
 
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+    let output = [];
+    let map = new Map();
+
+    transactions.forEach(transaction => {
+        if (map.has(transaction.category)) {
+            const count = map.get(transaction.category) + transaction.price;
+
+            map.set(transaction.category, count);
+        } else {
+
+            map.set(transaction.category, transaction.price)
+
+        }
+    });
+
+
+    map.forEach((value, key) => {
+
+            output.push({"category": key, "totalSpent": map.get(key)});
+
+        }
+    )
+
+    return output;
 }
 
 module.exports = calculateTotalSpentByCategory;
